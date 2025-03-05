c26d923fed8449fc884833277ad15398aed16046
Hedgehog-blue <d.spirina@ukma.edu.ua>
2025-03-05 13:05:15 +0200

    task05

---
+++ Labs/Labs03/task05.asm
@@ -0,0 +1,16 @@
+.model small
+.stack 100h
+
+.code
+main proc
+   
+    mov ds, ax      
+
+    mov dx, 1234h   ; to check the results in debug 
+    xor dx, dx      ;comparing each bit so that it is always 0 (xor)
+    ; xor sometimes faster than mov so it is a good way to replace mov 
+
+    mov ah, 4Ch     
+    int 21h
+main endp
+end main
