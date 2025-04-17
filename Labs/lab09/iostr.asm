01362d240e2fa1434e35b15c9264ebc4c764674f
Hedgehog-blue <d.spirina@ukma.edu.ua>
2025-04-17 15:28:31 +0300

    full task done

--- Labs/lab09/iostr.asm
+++ Labs/lab09/iostr.asm
@@ -37,35 +37,103 @@ input_loop:
 
     pop cx
     loop input_loop
+    jmp end_input
+
+    pop cx
+
+end_input:
+    call display_output
+
+    mov ax, 4C00h
+    int 21h
 
 main ENDP
 
 ;
 display_msg1 PROC
-
+    mov ah, 09h
+    mov dx, offset prompt_msg
+    int 21h
+    ret
 display_msg1 ENDP
 
 ;
 get_input PROC
-
+    mov bx, [position]
+    mov [pointers + si], bx
+    mov bx, [position]
 read_letters:
-
+    mov ah, 01h
+    int 21h
+    
+    
+    cmp al, 0Dh
+    je process_newline
+    
+    ; Store the character in the buffer
+    mov [bx], al
+    inc bx
+    jmp read_char_loop
 process_lines:
-
+    ; Read the Line Feed character (0Ah) that follows Enter
+    mov ah, 01h
+    int 21h
+    
+    ; Replace CRLF with $ (string terminator)
+    mov byte ptr [bx], '$'
+    inc bx
+    
+    ; Update the current position in the buffer
+    mov [current_pos], bx
+    
+    ret
 get_input ENDP
 ; 
 display_msg2 PROC
-
+    mov ah, 09h
+    mov dx, offset output_msg
+    int 21h
+    
+    ; Check if any strings were entered
+    mov cx, [string_count]
+    test cx, cx
+    jz end_output             ; Skip if no strings were entered
+    
+    ; Calculate the starting index for the last string
+    mov si, cx
+    dec si
+    shl si, 1  
 msg2_loop:
-
+    call display_string
+    
+    ; Move to the previous string
+    sub si, 2
+    loop output_loop
+    
+end_output:
+    ret
 display_msg2 ENDP 
 ;
 display_string PROC
-
+    mov dx, [pointers + si]
+    
+    ; Display the string
+    mov ah, 09h
+    int 21h
+    
+    ; Display CRLF
+    call display_newline
+    
+    ret
 display_string ENDP
 ;
 display_new PROC
-
+    mov dl, 0Dh
+    mov ah, 02h
+    int 21h
+    mov dl, 0Ah
+    int 21h
+    ret
 display_new ENDP
 
 
